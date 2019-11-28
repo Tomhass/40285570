dbe73d97d99833c8acffaef1837aa5920101f9e0
Thomas Heaton <40285570@live.napier,ac,uk>
2019-11-28 16:57:08 +0000

    test

---
+++ forms.py
@@ -0,0 +1,10 @@
+# LOGIN FORM FOR USERS
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import Datarequired
+
+class LoginForm(FlaskForm)
+	username = StringField('Username', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	remember_me = BooleanField('Remember Me')
+	submit = SubmitField('Sign In')
